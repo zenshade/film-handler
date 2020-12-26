0e044fa6e5cde29a40eb174a65b09f7d9ac753a8
Levi Conley <zenshade@gmail.com>
2020-12-26 13:45:41 -0500

    Added circleci

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [org.xerial/sqlite-jdbc "3.27.2"]]
   :plugins [[duct/lein-duct "0.12.1"]]
   :main ^:skip-aot film-ratings.main
+  :uberjar-name "film-ratings.jar"
   :resource-paths ["resources" "target/resources"]
   :prep-tasks     ["javac" "compile" ["run" ":duct/compiler"]]
   :middleware     [lein-duct.plugin/middleware]
