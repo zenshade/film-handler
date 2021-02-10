747ef7f8b514d1ae6279f661aa46d95a72626255
Levi Conley <zenshade@gmail.com>
2021-02-10 05:49:39 -0500

    add list films stuff

--- src/film_ratings/handler/film.clj
+++ src/film_ratings/handler/film.clj
@@ -14,8 +14,15 @@
     (let [film (reduce-kv (fn [m k v] (assoc m (keyword k) v))
                           {}
                           (dissoc film-form "__anti-forgery-token"))
-          result (boundary.film/create-film db film)
+          result (film-ratings.boundary.film/create-film db film)
           alerts (if (:id result)
                    {:messages ["Film added"]}
                    result)]
       [::response/ok (views.film/film-view film alerts)])))
+
+(defmethod ig/init-key :film-ratings.handler.film/list [_ {:keys [db]}]
+  (fn [_]
+    (let [films-list (film-ratings.boundary.film/list-films db)]
+      (if (seq films-list)
+        [::response/ok (views.film/list-films-view films-list {})]
+        [::response/ok (views.film/list-films-view [] {:messages ["No films found."]})]))))
